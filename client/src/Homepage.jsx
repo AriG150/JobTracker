8792ec0cdd1b73e903835fb3f5bb6fe5e10b39f2
Ari <gryphon150@gmail.com>
2019-12-08 13:36:42 -0800

    having many problems at Homepage.jsx. 1. Can't access Token so geting an error saying that i'm unauthorized. 2. 'apps' (from state) is undefined, so I can't find the length of it. I've checked via Postman that the user does have an application, so apps should have some length. Therefore I'm not using the hook correctly.

--- client/src/Homepage.jsx
+++ client/src/Homepage.jsx
@@ -1,26 +1,34 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import ApplicationsList from './ApplicationsList';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Link } from 'react-router-dom';
 import axios from 'axios';
-
 
 function Homepage(props) {
   const [apps, setApps] = useState([])
 
   useEffect(() => {
-    axios.get('/api/app')
+    let config = {
+      headers: {
+        Authorization: `Bearer ${props.token}`
+      }
+    }
+    axios.get('/api/app', config)
       .then((res) => {
         setApps(res.data)
       })
-  })
+  }, [])
 
   var mappedApps;
-  if(props.apps.length)
+  if(props.apps.length){
+    mappedApps = props.apps.map((app, id) => <div key={id}> {app.name} - {app.company} </div>)
+  } else {
+    mappedApps = <div> Start your job hunt! <Link to='/AddApp'>Add A New Job</Link>  </div>
+  }
 
   return (
-    <>
+    <div>
       <h1> Your jobs: </h1>
-    </>
+        {mappedApps}
+    </div>
   )
 }
 
