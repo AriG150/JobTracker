3d29a282c6c190496f0f70f48b56c57c5383d7d8
Ari <gryphon150@gmail.com>
2019-12-08 21:14:09 -0800

    creating AddApp.jsx page

--- client/src/AddApp.jsx
+++ client/src/AddApp.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
-function AddApp() {
-  
+function AddApp(props) {
+const [name, setName] = useState('')
+const [company, setCompany] = useState('')
+const [resume, setResume] = useState(false)
+const [coverLetter, setCoverLetter] = useState(false)
+const [recruiter, setRecruiter] = useState(false)
+const [informational, setInformational] = useState(false)
+const [offer, setOffer] = useState([])
+const [note, setNote] = useState([])
+const [submit, setSubmit] = useState()
+
+  let config = {
+    headers: {
+      Authorization: `Bearer ${props.token}`
+    }
+  };
+
 
 
   return (
@@ -9,10 +25,10 @@ function AddApp() {
         <h1>Start your Application: </h1>
         <div className="form-id">
           <form action="POST">
-            Job Title: <input type="text" name="title" /> <br />
-            Company Name: <input type="text" name="company" /> <br />
-            <input type="hidden" name="resume"/>
-            <input type="hidden" name="coverLetter"/>
+            Job Title: <input value={name} onChange={e => setName(e.target.value)} type="text" name="title" /> <br />
+            Company Name: <input value={company} onChange={e => setCompany(e.target.value)} type="text" name="company" /> <br />
+            <input value={resume} onChange={e => setResume(e.target.resume)} type="hidden" name="resume"/>
+            <input value={coverLetter} onChange  type="hidden" name="coverLetter"/>
             <input type="hidden" name="recruiter"/>
             <input type="hidden" name="informational"/>
             <input type="hidden" name="offer"/>
